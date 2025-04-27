9eb1f104d655da66fd7f5ff881c872e4e9f3b734
lpy <19545602064@163.com>
2025-04-27 13:16:52 +0800

    中午提交

--- 1.c
+++ 1.c
@@ -4,6 +4,14 @@ int main()
 {
 
 printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
+printf("我学会了分支管理\n");
 return 0;
 
 }
